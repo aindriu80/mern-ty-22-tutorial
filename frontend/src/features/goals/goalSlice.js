12a3e164ab3caa49dbd58476d7f1e942da6fba40
Aindriú Mac Giolla Eoin <aindriu80@gmail.com>
2022-03-10 13:41:09 +0000

    Fetch goals from server

--- frontend/src/features/goals/goalSlice.js
+++ frontend/src/features/goals/goalSlice.js
@@ -28,6 +28,25 @@ export const createGoal = createAsyncThunk(
   }
 )
 
+// Get user goals
+export const getGoals = createAsyncThunk(
+  'goals/getAll',
+  async (_, thunkAPI) => {
+    try {
+      const token = thunkAPI.getState().auth.user.token
+      return await goalService.createGoal(token)
+    } catch (error) {
+      const message =
+        (error.response &&
+          error.response.data &&
+          error.response.data.message) ||
+        error.message ||
+        error.toString()
+      return thunkAPI.rejectWithValue(message)
+    }
+  }
+)
+
 export const goalSlice = createSlice({
   name: 'goal',
   initialState,
@@ -49,10 +68,22 @@ export const goalSlice = createSlice({
         state.isError = true
         state.message = action.payload
       })
+      .addCase(getGoals.pending, (state) => {
+        state.isLoading = true
+      })
+      .addCase(getGoals.fulfilled, (state, action) => {
+        state.isLoading = false
+        state.isSuccess = true
+        state.goals = action.payload
+      })
+      .addCase(getGoals.rejected, (state, action) => {
+        state.isLoading = false
+        state.isError = true
+        state.message = action.payload
+      })
   },
 })
 
 export const { reset } = goalSlice.actions
-
 
 export default goalSlice.reducer
